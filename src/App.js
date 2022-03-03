ae4bc8ab4dd6e3f25f4e9ea66e4a0312fee0de95
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-03 12:17:56 +0100

    done class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Timer from "./components/Timer";
-// import TimerTwo from "./components/03-use-effect-mounting/TimerTwo";
-// import TimerThree from "./components/04-use-effect-unmounting/TimerThree";
-// import TimerFour from "./components/05-use-effect-conditional-updates/TimerFour";
-
-//import IronbnbList from './components/IronbnbList';  // <== IMPORT
+import IronbnbList from './components/IronbnbList';  // <== IMPORT
 
 function App() {
   const [show, setShow] = useState(false);
@@ -15,7 +10,7 @@ function App() {
       <button onClick={() => setShow(!show)}>
         { show ? "Hide" : "Show"}
       </button>
-      {show && <Timer />}
+      {show && <IronbnbList />}
     </div>
   );
 }
